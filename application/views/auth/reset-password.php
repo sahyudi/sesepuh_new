581424025fdd084dc1f0d0d3c2de4cdbbfa496d8
muhamad sahyudi <sahyudi.dev@gmail.com>
2019-11-02 07:51:46 +0700

    first time

---
+++ application/views/auth/reset-password.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>Reset Password &mdash; Sesepuh</title>
+
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/all.css">
+
+    <!-- CSS Libraries -->
+    <!-- <link rel="stylesheet" href="../node_modules/bootstrap-social/bootstrap-social.css"> -->
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/style.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/components.css">
+</head>
+
+<body>
+    <div id="app">
+        <section class="section">
+            <div class="container mt-5">
+                <div class="row">
+                    <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                        <div class="login-brand">
+                            <img src="<?= base_url('assets/') ?>img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
+                        </div>
+
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h4>Reset Password</h4>
+                            </div>
+
+                            <div class="card-body">
+                                <p class="text-muted">We will send a link to reset your password</p>
+                                <form method="POST">
+                                    <div class="form-group">
+                                        <label for="email">Email</label>
+                                        <input id="email" type="email" class="form-control" name="email" tabindex="1" required autofocus>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="password">New Password</label>
+                                        <input id="password" type="password" class="form-control pwstrength" data-indicator="pwindicator" name="password" tabindex="2" required>
+                                        <div id="pwindicator" class="pwindicator">
+                                            <div class="bar"></div>
+                                            <div class="label"></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="password-confirm">Confirm Password</label>
+                                        <input id="password-confirm" type="password" class="form-control" name="confirm-password" tabindex="2" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                            Reset Password
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="simple-footer">
+                            Copyright &copy; Sesepuh <?= date('Y') ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <!-- General JS Scripts -->
+    <script src="<?= base_url('assets/') ?>js/jquery-3.3.1.min.js"></script>
+    <script src="<?= base_url('assets/') ?>js/popper.min.js"></script>
+    <script src="<?= base_url('assets/') ?>js/bootstrap.min.js"></script>
+    <script src="<?= base_url('assets/') ?>js/jquery.nicescroll.min.js"></script>
+    <script src="<?= base_url('assets/') ?>js/moment.min.js"></script>
+    <script src="<?= base_url('assets/') ?>js/stisla.js"></script>
+
+    <!-- JS Libraies -->
+    <!-- <script src="../node_modules/jquery-pwstrength/jquery.pwstrength.min.js"></script>
+    <script src="../node_modules/selectric/public/jquery.selectric.min.js"></script> -->
+
+    <!-- Template JS File -->
+    <script src="<?= base_url('assets/') ?>js/scripts.js"></script>
+    <script src="<?= base_url('assets/') ?>js/custom.js"></script>
+    <script src="../assets/js/custom.js"></script>
+
+    <!-- Page Specific JS File -->
+</body>
+
+</html>
