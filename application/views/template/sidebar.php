4d9f5e9faac2d54375c1b9c33fce87372949d350
sahyudi_dev <poetrageranting@gmail.com>
2019-11-09 11:52:19 +0700

    pebraikan link nav bar

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -7,19 +7,19 @@
             <a href="index.html">SSI</a>
           </div>
           <ul class="sidebar-menu">
-              <li class="menu-header">Dashboard</li>
-              <li class="nav-item dropdown active">
-                <a href="#" class="nav-link"><i class="fas fa-fw fa-fire"></i><span>Dashboard</span></a>
-                <ul class="dropdown-menu">
-                  <li class="active"><a class="nav-link" href="<?= base_url('dashboard') ?>">Home</a></li>
-                </ul>
-              </li>
-            </ul>
+            <li class="menu-header">Dashboard</li>
+            <li class="nav-item dropdown active">
+              <a href="#" class="nav-link has-dropdown"><i class="fas fa-fw fa-fire"></i><span>Dashboard</span></a>
+              <ul class="dropdown-menu">
+                <li class="active"><a class="nav-link" href="<?= base_url('dashboard') ?>">Home</a></li>
+              </ul>
+            </li>
+          </ul>
 
-            <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-              <a href="<?= base_url('auth/logout') ?>" class="btn btn-danger btn-lg btn-block btn-icon-split">
-                <i class="fas fa-sign-out-alt"></i> Logout
-              </a>
-            </div>
+          <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+            <a href="<?= base_url('auth/logout') ?>" class="btn btn-danger btn-lg btn-block btn-icon-split">
+              <i class="fas fa-sign-out-alt"></i> Logout
+            </a>
+          </div>
         </aside>
       </div>
